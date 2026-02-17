c9d04807740b880c0a43df417a851a6c765327d2
Xander Cesari <xander@merriman.industries>
2026-02-16 19:24:30 -0800

    Added initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "mklittlefs-rs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          buildInputs = [
+            # Rust
+            rust-bin.stable.latest.default
+
+            # misc. libraries
+            pkg-config
+
+          ];
+
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      }
+    );
+}
